ac196adb9eb524731dad7945df06543f77fc959e
mEagledev <jaircarlos3302@gmail.com>
2023-06-23 18:25:52 -0500

    animation.fix()

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
         slideUpEnter: {
           "0%": {
             opacity: 0,
-            transform: "translateY(20px)",
+            transform: "translateY(-20px)",
           },
           "100%": {
             opacity: 100,
