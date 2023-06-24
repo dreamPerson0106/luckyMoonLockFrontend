8a8282c2773824ec3e51a8efee7340a532d806ad
mEagledev <jaircarlos3302@gmail.com>
2023-06-24 01:45:41 -0500

    Dialog.style.fix()

--- src/components/Dialog/index.js
+++ src/components/Dialog/index.js
@@ -29,42 +29,41 @@ const Dialog = ({ children, modalState, closeModal, button }) => {
     };
   }, [modal]);
 
-  if (modalState) {
-    return (
-      <div
-        tabIndex="-1"
-        aria-hidden="true"
-        className=" fixed top-0 left-0 right-0 z-20 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0  max-h-full bg-gray-800/60 backdrop-blur-md"
-      >
-        <div className=" flex justify-center w-full h-full max-h-[96%]">
-          <div className={`relative w-full max-w-2xl py-6`} ref={modal}>
-            <button
-              type="button"
-              className={`z-50 absolute top-8 right-2.5 text-gray-400 bg-transparent rounded-full hover:bg-[${background}] hover:text-[#FC5252] text-sm p-1.5 ml-auto inline-flex items-center`}
-              ref={closeBtn}
-              onClick={closeModal}
+  return (
+    <div
+      tabIndex="-1"
+      aria-hidden="true"
+      className={`${
+        modalState ? "animate-slideUpEnter" : "hidden"
+      } fixed top-0 left-0 right-0 z-20 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0  max-h-full bg-gray-800/60 backdrop-blur-md`}
+    >
+      <div className=" flex justify-center w-full h-full max-h-[96%]">
+        <div className={`relative w-full max-w-2xl py-6`} ref={modal}>
+          <button
+            type="button"
+            className={`z-50 absolute top-8 right-2.5 text-gray-400 bg-transparent rounded-full hover:bg-[${background}] hover:text-[#FC5252] text-sm p-1.5 ml-auto inline-flex items-center`}
+            ref={closeBtn}
+            onClick={closeModal}
+          >
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              <span className="sr-only">Close modal</span>
-            </button>
-            {children}
-          </div>
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <span className="sr-only">Close modal</span>
+          </button>
+          {children}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 };
-
 export default Dialog;
